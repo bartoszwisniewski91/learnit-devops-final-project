1dae825b4529fccc00dc930823d08c0fcc1dea7f
bartoszwisniewski91 <bartoszwisniewski91@gmail.com>
2026-03-18 23:29:29 +0000

    Test Git Action1

--- app/src/main/java/com/example/demo/HelloController.java
+++ app/src/main/java/com/example/demo/HelloController.java
@@ -1,12 +1,19 @@
 package com.example.demo;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@SpringBootApplication
 @RestController
-public class HelloController {
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
     @GetMapping("/")
-    public String hello() {
-        return "System DevOps Działa! V1.0";
+    public String home() {
+        return "Serwer Ubuntu + GitHub Actions działają!";
     }
 }
