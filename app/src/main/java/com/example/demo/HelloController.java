37cdd6140d1d97dca1c0769af57e2f25a79281fa
bartoszwisniewski91 <bartoszwisniewski91@gmail.com>
2026-03-18 23:25:52 +0000

    ver 1.0

---
+++ app/src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String hello() {
+        return "System DevOps Działa! V1.0";
+    }
+}
